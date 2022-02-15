73dc19df83bd6eb5b1b40aba14b4ffc7cccbfa88
Oscar Daniel <oscar.danielhz@gmail.com>
2022-02-15 09:58:42 -0600

    Addd MTEUserCredentials class

---
+++ mteframework/src/main/java/com/mte/mteframework/security/MTEUserCredentials.java
@@ -0,0 +1,67 @@
+package com.mte.mteframework.security;
+
+public class MTEUserCredentials
+{
+    public String Email;
+    public String Username;
+    public String Name;
+    public String Password;
+    public boolean LoggedIn;
+    public boolean IsNewUser;
+    public int UserId;
+    public String ObjectId;
+
+
+
+    //====================================================================
+    //====================================================================
+    public MTEUserCredentials()
+    {
+        this.Username = "Undefined";
+        this.Password = "null";
+        this.Email="user@domain.com";
+        this.LoggedIn = false;
+        this.IsNewUser =false;
+        this.UserId = -1;
+        this.ObjectId="";
+        this.Name = "Undefined";
+    }
+    //====================================================================
+    //====================================================================
+    public MTEUserCredentials(String uname, String password, String email)
+    {this.Username = uname;
+        this.Password = password;
+        this.Email = email;
+        this.LoggedIn = false;
+        this.IsNewUser =false;
+        this.UserId = -1;
+        this.ObjectId="";
+        this.Name = "Undefined";
+
+    }
+    //====================================================================
+    //====================================================================
+    public MTEUserCredentials(String name,String uname, String password, String email)
+    {this.Username = uname;
+        this.Password = password;
+        this.Email = email;
+        this.LoggedIn = false;
+        this.IsNewUser =false;
+        this.UserId = -1;
+        this.ObjectId="";
+        this.Name = name;
+
+    }
+    //====================================================================
+    //====================================================================
+    public void setLoggedIn()
+    {this.LoggedIn=true;}
+    //====================================================================
+    //====================================================================
+    public void setLoggedOut()
+    {
+        this.LoggedIn=false;
+    }
+    //====================================================================
+    //====================================================================
+}
